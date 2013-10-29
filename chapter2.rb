a2cc267d8bbad6a7d270f2f3b1f2612e0054df50
Abraham <asibaja@pernix-solutions.com>
2013-10-29 13:56:27 -0600

    Isolate Multiparameter Initialization

--- chapter2.rb
+++ chapter2.rb
@@ -1,56 +1,66 @@
 system('clear')
-class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(args)
-    args = defaults.merge(args)
-    @chainring  = args[:chainring]
-    @cog        = args[:cog]
-    @rim        = args[:rim]
-    @tire       = args[:tire]
-  end
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :rim, :tire
+    def initialize(chainring, cog, rim, tire)
+      @chainring  = chainring
+      @cog        = cog
+      @rim        = rim
+      @tire       = tire
+    end
 
-  def defaults
-    {:chainring => 40, :cog => 18}
-  end
+    def wheel
+      @wheel = SomeFramework::Wheel.new(rim, tire)
+    end
 
-  def wheel
-    @wheel = Wheel.new(rim, tire)
-  end
+    def ratio
+      chainring / cog.to_f
+    end
 
-  def ratio
-    chainring / cog.to_f
-  end
+    def diameter
+      wheel.diameter
+    end
 
-  def diameter
-    wheel.diameter
-  end
-
-  def gearInches
-    ratio * diameter
+    def gearInches
+      ratio * diameter
+    end
   end
 end
 
-class Wheel
-  attr_reader :rim, :tire
-  def initialize (rim, tire)
-    @rim = rim
-    @tire = tire
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(args[:chainring],
+                           args[:cog],
+                           args[:rim],
+                           args[:tire])
   end
-
-  Wheel = Struct.new(:rim, :tire)
-  def wheelify(data)
-    data.collect {|cell| Wheel.new(cell[0], cell[1])}
-  end
-
-  def diameter
-    rim + (tire * 2)
-  end
-
-  def circumference
-    diameter * Math::PI
-  end
-
 end
 
-puts Gear.new(:cog => 11, :rim => 26, :tire => 1.5).gearInches
+module SomeFramework
+  class Wheel
+      attr_reader :rim, :tire
+      def initialize (rim, tire)
+        @rim = rim
+        @tire = tire
+      end
 
+      Wheel = Struct.new(:rim, :tire)
+      def wheelify(data)
+        data.collect {|cell| Wheel.new(cell[0], cell[1])}
+      end
+
+      def diameter
+        rim + (tire * 2)
+      end
+
+      def circumference
+        diameter * Math::PI
+      end
+  end
+end
+
+puts GearWrapper.gear(
+  :chainring => 52,
+  :cog       => 11,
+  :rim       => 20,
+  :tire      => 1.5).gearInches
